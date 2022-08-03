4114c9cebcc96f269a92175a4aaa9b6f6e6a8fb0
pdvrostovSPb <pdvrostov@mail.ru>
2022-08-03 10:10:52 +0300

    added AlgorithmEfficience

---
+++ AlgorithmEfficiency/Program.cs
@@ -0,0 +1,36 @@
+﻿//Наименьший общий делитель НОД Алгоритм Евклида
+int n = 140;
+int m = 175;
+int count = n * m;
+while (n != m)
+{
+    if(n > m)
+        n = n - m;
+    else
+        m = m - n;
+    Console.WriteLine(n);
+    Console.WriteLine(m);
+    Console.WriteLine();
+}
+// Наименьшее общее кратное НОК(а,в) = а*в/НОД(а,в)
+Console.WriteLine(count/n);
+
+// constant algorithm
+
+/* int n = 0;
+if (n >= 0)
+    n++;
+else
+    n*=2; */
+
+// linear algorithm
+
+/* int n = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= n; i++)
+{
+    result = result * i;
+}
+Console.WriteLine(result); */
+
+
